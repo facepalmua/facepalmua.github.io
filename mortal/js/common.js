d1fcba6b53ab348eb0ff52e71073aa921e058a7f
facepalmua <facepalmua@gmail.com>
2018-03-19 07:41:09 +0200

    mortal

---
+++ mortal/js/common.js
@@ -0,0 +1,15 @@
+
+
+$(document).ready(function(){
+  $('.slider-1').slick({
+    slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrows: true,
+  dots: true
+  });
+});
+	
+
+
